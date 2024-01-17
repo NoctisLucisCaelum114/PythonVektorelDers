52835228f3d7401282b0c4a87090191257627ea5
NoctisLucisCaelum114 <muzikgurmesi71@gmail.com>
2024-01-17 20:02:04 +0300

    Consol

--- Konsol projem.py
+++ Konsol projem.py
@@ -1,7 +1,6 @@
-import Konsolprojem.Konsol
+import CAGRI_PROJE  # Import the module directly
 import oyunlar.oyun
 import animeönerileri.Animeönerileri
-import CAGRI_PROJE
 
 def anamenu():
     print("\033[31m")
@@ -19,17 +18,12 @@ def anamenu():
     secim = input("Seçiminiz: ")
 
     if secim == "1":
-        Konsolprojem.Konsol.hmmenu()
-        anamenu()
+        CAGRI_PROJE.hmmenu()  # Call the function directly
     elif secim == "2":
-       oyunlar.oyun.oyunmenu()
+        oyunlar.oyun.oyunmenu()
     else:
         print("Geçersiz seçim. Lütfen tekrar deneyin")
     if secim == "5":
         animeönerileri.Animeönerileri.animemenu()
-    # 201 ╔ # 205 ═ # 187 ╗ # 186 ║ # 200 ╚
-    # 188 ╝
-    
+# Call the function to start the menu
 anamenu()
-    ##hesapmakinesi ()
-    #print("╔"+"="*20+"╗"
---
+++ animeönerileri/Animeönerileri.py
@@ -0,0 +1,14 @@
+def animemenu():
+    print("\033[31m")
+    print("╔═════════════════════════╗")
+    print("║\033[31m                 ║")
+    print("║                         ║")
+    print("║  1-Doğaüstü güçler      ║")
+    print("║  2-Aksiyon              ║")
+    print("║  3-Dram                 ║")
+    print("║  4-Mecha                ║")
+    print("║  5-İsekai               ║")
+    print("║    Seçiminiz nedir?     ║")
+    print("╚═════════════════════════╝")
+
+    secim = input("Seçiminiz: ")
