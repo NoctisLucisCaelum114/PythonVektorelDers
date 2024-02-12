faca4d52f1c42c04bfe76482efe64cfd15167f5b
NoctisLucisCaelum114 <muzikgurmesi71@gmail.com>
2024-02-12 19:37:07 +0300

    Widget

---
+++ Layout.py
@@ -0,0 +1,15 @@
+from PyQt6.QtWidgets import *
+aa = QApplication([])
+
+ww = QWidget()
+
+icerik = QVBoxLayout()
+
+icerik.addWidget(QPushButton('Tıkla'))
+icerik.addWidget(QPushButton('Dene'))
+icerik.addWidget(QLabel('Bilgi'))
+
+ww.setLayout(icerik)
+
+ww.show()
+aa.exec()
---
+++ Masaüstü.py
@@ -0,0 +1,11 @@
+from PyQt6.QtWidgets import *
+app = QApplication([])
+Window = QWidget()
+Window.show()
+abc = QWidget()
+
+abc.show()
+app.exec()
+app = QApplication({})
+Window = QWidget()
+Window.show()
